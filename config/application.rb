3c9044b9ac23d9fd8965ea6b53542699955b20a0
rtm29331 <115970853+rtm29331@users.noreply.github.com>
2022-12-06 11:55:56 +0900

    更新

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,4 @@ module Proto
     # the framework and any gems in your application.
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get "user/:id", :to => "pages#show" # この1行を編集
+  devise_for :users
 end
